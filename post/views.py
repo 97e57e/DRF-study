26abe437f574b55f52e9aeef175bdb335f324a54
gardenLee <khjvvv7@gmail.com>
2019-10-12 23:26:21 +0900

    Mixins 상속
    template method patterns
    
    get, post, put, delete 메소드 상속받아 작성

--- post/views.py
+++ post/views.py
@@ -7,46 +7,79 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
 
+from rest_framework import generics
+from rest_framework import mixins
+
 # CBV
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
-class PostList(APIView):
-    def get(self, request):
-        posts = Post.objects.all()
-        serializer = PostSerializer(posts, many=True) # 다수의 쿼리셋을 시리얼라이징 할때는 many=True
-        return Response(serializer.data)
+############# Mixins 상속 ##################
+class PostList(mixins.ListModelMixin, mixins.CreateModelMixin,
+generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def post(self, request):
-        serializer = PostSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.list(request, *args, **kwargs)
 
-class PostDetail(APIView):
-    def get_object(pk):
-        try:
-            return Post.objects.get(pk=pk)
-        except Post.DoestNotExist:
-            raise Http404
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
 
-    def get(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post)
-        return Response(serializer.data)
+class PostDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,
+mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
 
-    def put(self, request, pk, format=None):
-        post = self.get_object(pk)
-        serializer = PostSerializer(post, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
 
 
-    def delete(self, request, pk, format=None):
-        post = self.get_object(pk)
-        post.delete()
-        return Response(status=status.HTTP_204_NO_CONNECTED)
+
+############# API View 상속 ##################
+# class PostList(APIView):
+#     def get(self, request):
+#         posts = Post.objects.all()
+#         serializer = PostSerializer(posts, many=True) # 다수의 쿼리셋을 시리얼라이징 할때는 many=True
+#         return Response(serializer.data)
+
+#     def post(self, request):
+#         serializer = PostSerializer(data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_201_CREATED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+# class PostDetail(APIView):
+#     def get_object(pk):
+#         try:
+#             return Post.objects.get(pk=pk)
+#         except Post.DoestNotExist:
+#             raise Http404
+
+#     def get(self, request, pk, format=None):
+#         print("detail get 요청")
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post)
+#         return Response(serializer.data)
+
+#     def put(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         serializer = PostSerializer(post, data=request.data)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#     def delete(self, request, pk, format=None):
+#         post = self.get_object(pk)
+#         post.delete()
+#         return Response(status=status.HTTP_204_NO_CONNECTED)
